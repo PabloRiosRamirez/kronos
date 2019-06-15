732ca8a72c26cb13ed762f6ff60a2588b79108dd
Pablo Rios Ramirez <pa.riosramirez@gmail.com>
2019-06-15 15:22:51 -0400

    First commit

---
+++ src/main/java/online/grisk/zeus/ZeusApplication.java
@@ -0,0 +1,15 @@
+package online.grisk.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ZeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeusApplication.class, args);
+	}
+
+}
