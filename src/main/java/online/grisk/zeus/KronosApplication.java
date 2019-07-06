60953357f6da506c77ecc7d19d0bf76b411be518
Pablo Rios Ramirez <pa.riosramirez@gmail.com>
2019-07-06 10:24:47 -0400

    Update Artifact

--- src/main/java/online/grisk/zeus/KronosApplication.java
+++ src/main/java/online/grisk/zeus/KronosApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class ZeusApplication {
+public class KronosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZeusApplication.class, args);
+		SpringApplication.run(KronosApplication.class, args);
 	}
 
 }
