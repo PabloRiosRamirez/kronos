b8b36eba06ce6022c3d7ad04f9a7520664edfda5
Pablo Rios Ramirez <pa.riosramirez@gmail.com>
2019-07-13 10:23:28 -0400

    Upload coverage

--- src/test/java/online/grisk/kronos/KronosApplicationTests.java
+++ src/test/java/online/grisk/kronos/KronosApplicationTests.java
@@ -8,9 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KronosApplicationTests {
-
+    KronosApplication kronosApplication = new KronosApplication();
     @Test
     public void contextLoads() {
+        KronosApplication.main(new String[0]);
     }
-
 }
