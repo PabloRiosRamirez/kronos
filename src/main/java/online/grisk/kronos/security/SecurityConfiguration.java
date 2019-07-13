140b332873be219975f9611afcd65c4ef2486869
Pablo Rios Ramirez <pa.riosramirez@gmail.com>
2019-07-13 12:58:46 -0400

    Revert "Securiting the dashboard and services consumed"
    This reverts commit 0320a161f28cc74c9908961f7fe65401f4a41ebe.

--- src/main/java/online/grisk/kronos/security/SecurityConfiguration.java
+++ src/main/java/online/grisk/kronos/security/SecurityConfiguration.java
@@ -1,18 +1,14 @@
 package online.grisk.kronos.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-@Order(1)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Value("${SECRET_USER}")
@@ -20,18 +16,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value("${SECRET_PASS}")
     String pass;
 
-    @Autowired
-    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser(user).password("{noop}".concat(pass)).roles("SYSTEM");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                .and().requestMatchers().antMatchers("/eureka/**")
-                .and().authorizeRequests().antMatchers("/eureka/**")
-                .hasRole("SYSTEM").anyRequest().denyAll().and()
-                .httpBasic().and().csrf().disable();
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .authenticated()
+                .and()
+                .csrf().disable();
     }
 }
